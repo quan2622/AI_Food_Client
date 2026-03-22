b7bd55fc569aaf50b39841956f1d111909c2d83f
Quan Nguyen Hong <nhq367821@gmail.com>
2026-03-22 23:16:37 +0700

    add shadcn ui & zustand

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-   <main className="text-cyan-400 p-4 border-2 border-amber-400">
-    Hello this is project food_ai
-   </main>
+    <main className="space-y-4">
+      <p className="text-cyan-400 p-4 border-2 border-amber-400">
+        Hello this is project food_ai
+      </p>
+
+      <Button variant={"destructive"}>Kill button</Button>
+    </main>
   );
 }
